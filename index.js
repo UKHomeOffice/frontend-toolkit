5fd304132f156f9a1866749fffa0ea945cc2a09c
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2018-09-03 13:06:11 +0100

    move assets into assets dir

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const path = require('path');
 
 module.exports = {
   views: path.resolve(__dirname, './templates'),
-  images: path.resolve(__dirname, './images')
+  assets: path.resolve(__dirname, './assets')
 };
